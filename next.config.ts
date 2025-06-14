3cab6e198cf31bd1aa64233f11eb3a12cde1a72e
eeennsu <diso592@naver.com>
2025-06-14 11:57:50 +0900

    fix: next config 변경

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/eunsu-resume',
+  assetPrefix: '/eunsu-resume',
 };
 
 export default nextConfig;

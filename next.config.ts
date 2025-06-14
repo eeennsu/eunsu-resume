756bc785d5018199c4700d06205bb2546cacb818
eeennsu <diso592@naver.com>
2025-06-14 22:39:45 +0900

    fix: next config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next';
 
+const debug = process.env.NODE_ENV !== 'production';
+const repository = 'eunsu-resume';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/eunsu-resume',
-  assetPrefix: '/eunsu-resume',
+  basePath: debug ? '' : `/${repository}`,
+  assetPrefix: debug ? '' : `/${repository}`,
 };
 
 export default nextConfig;
